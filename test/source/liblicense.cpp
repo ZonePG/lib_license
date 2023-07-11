3ee3931b62e2ed5be33583bb99633f5f5df64a0a
ZonePG <zonepg666@gmail.com>
2023-07-11 15:39:42 +0800

    update license test

--- test/source/liblicense.cpp
+++ test/source/liblicense.cpp
@@ -14,6 +14,13 @@ TEST_CASE("encrypt license") {
     file.close();
 }
 
+TEST_CASE("dencrypt license") {
+    std::ifstream rFile("license.lic");
+    LicenseCrypto rLicenseCrypto(rFile);
+    auto result = rLicenseCrypto.Check("CPU序列号1", "磁盘序列号2", "场景名称3", "场景版本号4");
+    CHECK(result.first);
+}
+
 TEST_CASE("encrypt license mac fixed") {
     License license("08:bf:b8:19:39:2e", "CPU序列号1", "磁盘序列号2", "场景名称3", "场景版本号4", 2);
     LicenseCrypto licenseCrypto(std::make_shared<License>(license));
@@ -23,6 +30,13 @@ TEST_CASE("encrypt license mac fixed") {
     file.close();
 }
 
+TEST_CASE("dencrypt license mac failed") {
+    std::ifstream rFile("license_mac_fixed.lic");
+    LicenseCrypto rLicenseCrypto(rFile);
+    auto result = rLicenseCrypto.Check("CPU序列号1", "磁盘序列号2", "场景名称3", "场景版本号4");
+    CHECK(!result.first);
+}
+
 TEST_CASE("encrypt license outdated") {
     License license("08:bf:b8:19:39:2f", "CPU序列号1", "磁盘序列号2", "场景名称3", "场景版本号4", 0);
     LicenseCrypto licenseCrypto(std::make_shared<License>(license));
@@ -30,20 +44,6 @@ TEST_CASE("encrypt license outdated") {
     std::ofstream file("license_outdated.lic");
     file << licenseCrypto;
     file.close();
-}
-
-TEST_CASE("dencrypt license") {
-    std::ifstream rFile("license.lic");
-    LicenseCrypto rLicenseCrypto(rFile);
-    auto result = rLicenseCrypto.Check("CPU序列号1", "磁盘序列号2", "场景名称3", "场景版本号4");
-    CHECK(result.first);
-}
-
-TEST_CASE("dencrypt license mac failed") {
-    std::ifstream rFile("license_mac_fixed.lic");
-    LicenseCrypto rLicenseCrypto(rFile);
-    auto result = rLicenseCrypto.Check("CPU序列号1", "磁盘序列号2", "场景名称3", "场景版本号4");
-    CHECK(!result.first);
 }
 
 TEST_CASE("dencrypt outdated license") {
